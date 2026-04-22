f3107352f15a691ac599f730f2d72c9f5e525b91
shubhamsawant1 <shubhamsawant22011@gmail.com>
2026-04-22 20:20:33 +0530

    basic cpp

---
+++ BASIC_CPP/basic.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+  main() {        /* genric skeltopn of c++ code */
+     // int
+     int x = 10;
+     int y = 12;
+     int total = x + y;
+     cout<< total << endl;
+     // long  long long 
+      long k = 15;
+      cin >> k;
+      cout<< k << endl;
+      long long p = 1850000000;
+       cout << p << endl;
+      // double  float 
+
+      float PI = 3.14; // decimal value
+      cout<< PI << endl;
+      float v = 5; // it also store integer flaot stores decimal values but it does't mean it cant store integer
+     
+      cout<< "flaot value of v: " << v << endl;
+
+
+    cout<<"shubham: "<< total << endl;
+
+ return 0;
+}
