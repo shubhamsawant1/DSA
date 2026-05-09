ab1fc9f11496b6ba5e5825248c188bd80aacf1d6
shubhamsawant1 <shubhamsawant22011@gmail.com>
2026-05-09 10:56:41 +0530

    time complexity

--- BASIC_CPP/time-comp/timecomp.cpp
+++ BASIC_CPP/time-comp/timecomp.cpp
@@ -2,7 +2,16 @@
 using namespace std;
 
 int main() {
+    int n = 5;
+    int i = 0;
 
-
+    for (i = 0; i < n; i += 1) {
+        for (int j = 0; j <= i; j += 1){
+            cout<< j;
+        }
+    }
+      
     return 0;
 }
+
+// output: 0 01 012 0123 01234  time complexity = O (n * 2)
